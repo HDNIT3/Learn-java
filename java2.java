1891457bf5b2d433b71707aeffc8384424b0c085
HDNIT3 <nguyenhuynh.663459@gmail.com>
2024-02-11 16:22:07 +0700

    ngay gio trong java

---
+++ java2.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+import java.util.Calendar;
+
+public class java2 {
+    public static void main(String[] args) {
+        Calendar date1 = Calendar.getInstance();
+        Date k = date1.getTime();   
+        System.out.println("hien gioi las "+k);
+    }
+}
