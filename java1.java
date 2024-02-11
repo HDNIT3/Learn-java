28cf625df1f39432303575192b9f9cbac6cfba95
HDNIT3 <nguyenhuynh.663459@gmail.com>
2024-02-11 07:37:30 +0700

    7:37 2/11/2024

--- java1.java
+++ java1.java
@@ -1,8 +1,28 @@
-/**
- * java1
- */
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+// so sánh hashset với treeset trong java
 public class java1 {
-   public static void main(String[] args) {
-      System.out.println("hello");
-   }
+  public static void main(String[] args) {
+    Set<Integer> set = new HashSet<Integer>();
+    set.add(1);
+    set.add(2);
+    set.add(1);
+    set.add(6);
+    set.add(5);
+    set.add(2);
+    
+    System.out.println(set);
+
+    Set<Integer> set2 = new TreeSet<Integer>();
+    set2.add(1);
+    set2.add(2);
+    set2.add(1);
+    set2.add(6);
+    set2.add(5);
+    set2.add(2);
+    
+    System.out.println(set2);
+  }
 }
