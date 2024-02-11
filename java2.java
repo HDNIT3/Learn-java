9398a51e7bcb9f1e5c5c7bcb605b00465f94247b
HDNIT3 <nguyenhuynh.663459@gmail.com>
2024-02-11 16:29:22 +0700

    daa

--- java2.java
+++ java2.java
@@ -5,6 +5,16 @@ public class java2 {
     public static void main(String[] args) {
         Calendar date1 = Calendar.getInstance();
         Date k = date1.getTime();   
-        System.out.println("hien gioi las "+k);
+        System.out.println("hien gioi la: "+k);
+
+        System.out.println();
+
+        System.out.println(date1.get(Calendar.DAY_OF_MONTH));
+        System.out.println(date1.get(Calendar.MONTH));
+        System.out.println(date1.get(Calendar.YEAR));
+
+        System.out.println(Calendar.HOUR);
+        System.out.println(date1.get(Calendar.MONTH));
+        System.out.println(date1.get(Calendar.YEAR));
     }
 }
